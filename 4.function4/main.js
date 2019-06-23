8ba08a443c337c8d0f77cec4b62baaf0e4beeb28
otaGran <ujt2h8@gmail.com>
2019-06-23 18:35:17 +0800

    Create main.js

---
+++ 4.function4/main.js
@@ -0,0 +1,6 @@
+function countWords(message){
+    // wirte your code here
+    var codes = message.split(" ")
+    console.log(codes.length)
+}
+countWords('Good morning, I love JavaScript.'); // should return 5
